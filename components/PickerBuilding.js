6d238d534468822a55971bf06a40d3996b6d5be3
jelbo96 <ijelvezh@gmail.com>
2021-08-28 21:02:36 -0400

    pickers from objects

--- components/PickerBuilding.js
+++ components/PickerBuilding.js
@@ -6,6 +6,30 @@ const PickerBuilding = ({
   selectedBuilding,
   setSelectedBuilding,
 }) => {
+  let buildings = [
+    { key: 1, value: "1k", name: "1000 (Decanatura)" },
+    { key: 2, value: "2k", name: "2000 (Naval)" },
+    { key: 3, value: "3k", name: "3000" },
+    { key: 4, value: "4k", name: "4000" },
+    { key: 5, value: "5k", name: "5000 (Casa Haverbeck)" },
+    { key: 6, value: "6k", name: "6000 (Cs. Basicas)" },
+    { key: 7, value: "7k", name: "7000" },
+    { key: 8, value: "8k", name: "8000" },
+    { key: 9, value: "9k", name: "9000" },
+    { key: 10, value: "10k", name: "10000 (Informática)" },
+    { key: 11, value: "11k", name: "11000 (Obras Civiles - Construcción)" },
+    { key: 12, value: "12k", name: "12000 (Biblioteca)" },
+    { key: 13, value: "13k", name: "13000 (Industrial)" },
+    { key: 14, value: "14k", name: "14000" },
+    { key: 15, value: "100", name: "100 (Canal de Ensayos)" },
+    { key: 16, value: "200", name: "200" },
+    { key: 17, value: "400", name: "400" },
+    { key: 18, value: "500", name: "500 (LEMCO)" },
+    { key: 19, value: "Casino", name: "Casino" },
+    { key: 20, value: "Gimnasio", name: "Gimnasio" },
+    { key: 21, value: "Estadio", name: "Estadio" },
+  ];
+
   return (
     <Picker
       style={{
@@ -19,16 +43,13 @@ const PickerBuilding = ({
       selectedValue={selectedBuilding}
       onValueChange={(itemValue, itemIndex) => setSelectedBuilding(itemValue)}
     >
-      <Picker.Item label="1000" value="1k" />
-      <Picker.Item label="2000" value="2k" />
-      <Picker.Item label="3000" value="3k" />
-      <Picker.Item label="4000" value="4k" />
-      <Picker.Item label="5000" value="5k" />
-      <Picker.Item label="6000" value="6k" />
-      <Picker.Item label="7000" value="7k" />
-      <Picker.Item label="8000" value="8k" />
-      <Picker.Item label="9000" value="9k" />
-      <Picker.Item label="10000 (Informatica)" value="10k" />
+      {buildings.map((building) => (
+        <Picker.Item
+          key={building.key}
+          label={building.name}
+          value={building.value}
+        />
+      ))}
     </Picker>
   );
 };
--- components/PickerTemperature.js
+++ components/PickerTemperature.js
@@ -6,6 +6,18 @@ const PickerTemperature = ({
   selectedTemperature,
   setSelectedTemperature,
 }) => {
+  let temperatures = [
+    { key: 1, value: "34", name: "34 °C" },
+    { key: 2, value: "34.5", name: "34.5 °C" },
+    { key: 3, value: "35", name: "35 °C" },
+    { key: 4, value: "35.5", name: "35.5 °C" },
+    { key: 5, value: "36", name: "36 °C" },
+    { key: 6, value: "36.5", name: "36.5 °C" },
+    { key: 7, value: "37", name: "37 °C" },
+    { key: 8, value: "37.5", name: "37.5 °C" },
+    { key: 9, value: "38", name: "38 °C" },
+    { key: 10, value: ">38", name: ">38 °C" },
+  ];
   return (
     <Picker
       style={{
@@ -21,16 +33,9 @@ const PickerTemperature = ({
         setSelectedTemperature(itemValue)
       }
     >
-      <Picker.Item label="34 °C" value="34" />
-      <Picker.Item label="34.5 °C" value="34.5" />
-      <Picker.Item label="35 °C" value="35" />
-      <Picker.Item label="35.5 °C" value="35.5" />
-      <Picker.Item label="36 °C" value="36" />
-      <Picker.Item label="36.5 °C" value="36.5" />
-      <Picker.Item label="37 °C" value="37" />
-      <Picker.Item label="37.5 °C" value="37.5" />
-      <Picker.Item label="38 °C" value="38" />
-      <Picker.Item label=">38 °C" value=">38" />
+      {temperatures.map((temp) => (
+        <Picker.Item key={temp.key} label={temp.name} value={temp.value} />
+      ))}
     </Picker>
   );
 };
